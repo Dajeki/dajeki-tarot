9bf303f48bd9479b896be0bc85552eca6544763f
Zachary A. K <32288262+Dajeki@users.noreply.github.com>
2024-10-03 20:39:19 -0400

    Setup initial moon phase component
     [Resources]: Add SVG icons for the 8 moon phases
     [Feature Progress]: Start working on the moon phase component

---
+++ app/components/moon-phase.tsx
@@ -0,0 +1,5 @@
+export default function MoonPhase() {
+  return (
+	<div className="font-serif">moon-phase</div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		fontFamily: {
+			sans: "Rubik",
+			serif: "Permanent Marker",
+			card: "Mitr"
+	  },
     extend: {
       colors: {
         background: "var(--background)",
